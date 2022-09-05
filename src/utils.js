ac1c9513720654d0bfbeb2224b26a9078141213c
pikpokjeon <pikpokjeon@gmail.com>
2022-09-05 21:34:31 +0900

    Lib 요소생성 요소특성부여

--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,38 @@ export const is = typeOf.reduce((typeObj, type) => Object.assign(typeObj, { [typ
 
 // true
 // const a = is['array']( [2, 3, 4, 5,] )
+
+export const isChildren = data => is['array']( data ) || is['string']( data ) || 'nodeName' in data 
+
+export const setAttr = (type, el, attr={} ) => 
+{
+    const attributes = Object.entries(attr)
+    if ( is['array']( attr ) )
+    {
+        return attributes.reduce( ( acc, [key, val] ) =>
+        {
+            if(key === 'text') acc.appendChild(document.createTextNode(val))
+            acc.setAttribute(key,val)
+            return acc
+        },el )    
+    }else
+}
+
+export const updateChildren = ( type, parent, children = [] ) =>
+{
+    if ( is['undefined']( children ) ) return parent
+    if ( !is['array']( children ) ) children = [...children]
+    children.reduce( ( acc, cur ) =>
+    {
+        acc.appendChild( cur )
+        return acc
+    },parent)
+    
+}
+
+export const createElement = ( type ) => ( tag, attr = {}, children=[] ) =>
+{
+    const el = type === 'svg' ?  document.createElementNS('http://www.w3.org/2000/svg', tag) : document.createElement(tag)
+    setAttr( type, el, attr )
+    
+}
