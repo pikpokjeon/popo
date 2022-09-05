c81ffb0d82b5558d2ad496369c9003fa2737d54f
pikpokjeon <pikpokjeon@gmail.com>
2022-09-05 20:37:55 +0900

    INIT COMMIT

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import {nodeResolve} from "@rollup/plugin-node-resolve"
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: "./src/index.js",
+  plugins: [nodeResolve(), babel({babelHelpers: 'bundled'}), commonjs()],
+  output: [
+    {
+      name: 'Popo',
+      dir: "dist/cjs",
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      name: 'Popo',
+      dir: "dist/umd",
+      format: "umd",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      name: 'Popo',
+      dir: "dist/es",
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+}
