c99929962761a28382421535a2436767323121b7
pikpokjeon <pikpokjeon@gmail.com>
2022-09-27 13:08:56 +0900

    MOVE 코드 분할

--- src/lib.js
+++ src/lib.js
@@ -1,24 +1,68 @@
+import {is, isChildren} from './utils.js'
+
+export const setAttr = ( type, el, attr ) => 
+{
+    const attributes = Object.entries( attr )
+    if ( is['array']( attributes ) )
+    {
+        return attributes.reduce( ( acc, [key, val] ) =>
+        {
+            if ( key === 'text' )
+            {
+              acc.appendChild(document.createTextNode(val))  
+            } 
+            else acc.setAttribute(key,val)
+            return acc
+        },el )    
+    }
+    return el
+}
+
+export const removeChildren = parent => 
+{
+    while ( parent.firstChild )
+    {
+        parent.removeChild(parent.firstChild)
+    }
+    return parent
+}
+
+
+export const updateChildren = ( type, parent, children = [] ) =>
+{
+    if ( is['undefined']( children ) ) return parent
+    if ( !is['array']( children ) ) children = [children]
+    children.reduce( ( acc, cur ) =>
+    {
+        acc.appendChild( cur )
+        return acc
+    }, parent )
+    return parent
+    
+}
 
 
 
-// DOM 엘리먼트 생성
+export const createElement =  type  => ( tag, attr = {}, children=[] ) =>
+{
+    const el = type === 'svg' ?  document.createElementNS('http://www.w3.org/2000/svg', tag) : document.createElement(tag)
+    if ( isChildren( attr ) )
+    {
+        updateChildren(type,el,children)
+        return el
+    }
+    setAttr( type, el, attr )
+    updateChildren(type,el,children)
+    return el
+    
+}
+
+export const element = type => tag => ( attr ={}, children=[] ) =>
+{
+    const elByType = createElement( type )
+    return isChildren( attr ) ? elByType( tag, {},attr) : elByType(tag,attr,children)
+}
 
 
+export const fragment = (children) => setChildren(document.createDocumentFragment(), children);
 
-// DOM 속성 적용
-
-// DOM 트리 생성
-
-// DATA변화시 리렌더링
-
-// 상태
-
-// 저장
-
-// 변경
-
-// 변경 감지
-
-// 변경 후 업데이트
-
-// 가져오기
--- src/utils.js
+++ src/utils.js
@@ -17,79 +17,12 @@ const initType =
     html: el => /<\/?[a-z][\s\S]*>/i.test( el )
  }
 
-export const is = typeOf.reduce((typeObj, type) => Object.assign(typeObj, { [type]: d => typeof d === type }), { ...initType })
-
 // true
 // const a = is['array']( [2, 3, 4, 5,] )
+export const is = typeOf.reduce((typeObj, type) => Object.assign(typeObj, { [type]: d => typeof d === type }), { ...initType })
+
 
 export const isChildren = data => is['array']( data ) || is['string']( data ) || 'nodeName' in data 
-
-export const setAttr = (type, el, attr ) => 
-{
-    const attributes = Object.entries( attr )
-    if ( is['array']( attributes ) )
-    {
-        return attributes.reduce( ( acc, [key, val] ) =>
-        {
-            if ( key === 'text' )
-            {
-              acc.appendChild(document.createTextNode(val))  
-            } 
-            else acc.setAttribute(key,val)
-            return acc
-        },el )    
-    }
-    return el
-}
-
-export const removeChildren = parent => 
-{
-    while ( parent.firstChild )
-    {
-        parent.removeChild(parent.firstChild)
-    }
-    return parent
-}
-
-
-export const updateChildren = ( type, parent, children = [] ) =>
-{
-    if ( is['undefined']( children ) ) return parent
-    if ( !is['array']( children ) ) children = [children]
-    children.reduce( ( acc, cur ) =>
-    {
-        acc.appendChild( cur )
-        return acc
-    }, parent )
-    return parent
-    
-}
-
-
-
-export const createElement =  type  => ( tag, attr = {}, children=[] ) =>
-{
-    const el = type === 'svg' ?  document.createElementNS('http://www.w3.org/2000/svg', tag) : document.createElement(tag)
-    if ( isChildren( attr ) )
-    {
-        updateChildren(type,el,children)
-        return el
-    }
-    setAttr( type, el, attr )
-    updateChildren(type,el,children)
-    return el
-    
-}
-
-export const element = type => tag => ( attr ={}, children=[] ) =>
-{
-    const elByType = createElement( type )
-    return isChildren( attr ) ? elByType( tag, {},attr) : elByType(tag,attr,children)
-}
-
-
-export const fragment = (children) => setChildren(document.createDocumentFragment(), children);
-
 
 export const setObj = ( {storage, key, data} ) =>
 {
